d827f0d771fc934e4264886326133a6a31708611
C16735211 <C16735211@mydit.ie>
2020-02-17 12:13:42 +0000

    users app for registration

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import include, url
 from django.urls import path, include
 from . import views
+from django.contrib import admin
+from django.urls import path
+from users import views as user_views
 
 app_name = 'app'
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register'),
     url(r'^$', views.MainPage.as_view(), name='home'),
     url(r'^incidences/$', views.incidences_datasets, name='incidences'),
     path(r'', include('pwa.urls')),
--- app/views.py
+++ app/views.py
@@ -3,11 +3,18 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import HttpResponse
 from .models import Incidences
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 class MainPage(TemplateView):
     template_name = 'app/base.html'
+
+
+@login_required
+def home(request):
+    return render(request, "home.html", {})
 
 
 def incidences_datasets(request):
