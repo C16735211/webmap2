bc9e31bc74afe89b22b48fbaf51e8637012c5f54
C16735211 <C16735211@mydit.ie>
2020-03-22 14:00:27 +0000

    adding reviews to blogs - demo of reset password

--- blog/views.py
+++ blog/views.py
@@ -23,7 +23,7 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']  # post the latest posts in the blog
-    paginate_by = 3
+    paginate_by = 10
 
 
 class UserPostListView(ListView):
